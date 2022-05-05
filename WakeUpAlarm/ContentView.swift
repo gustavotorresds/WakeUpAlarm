c72e9c41a5a5debbd10c72dcf3c074efedbe1468
Gustavo Torres <gustavotorresds@gmail.com>
2022-05-05 18:59:09 -0300

    Initial Commit

---
+++ WakeUpAlarm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WakeUpAlarm
+//
+//  Created by Gustavo Torres on 5/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WakeUpAlarm/WakeUpAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeUpAlarmApp.swift
+//  WakeUpAlarm
+//
+//  Created by Gustavo Torres on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WakeUpAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
