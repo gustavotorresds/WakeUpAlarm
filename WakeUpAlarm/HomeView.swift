94c5284ad4e3faaed75a5893e6e49319b38b9a95
Arjun Mangla <arjunmangla96@gmail.com>
2022-05-15 12:25:31 -0700

    basic interaction elements for homeview

--- WakeUpAlarm/HomeView.swift
+++ WakeUpAlarm/HomeView.swift
@@ -10,7 +10,34 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("HOME")
+        
+        VStack(spacing: 20) {
+            Text("Wake-up Alarm")
+                .font(.system(size: 48).bold())
+                .padding(.bottom)
+            Text("Tomorrow I will")
+                .font(.system(size: 32))
+            HStack {
+                Text("wake up at")
+                    .font(.system(size: 32))
+                Button(action: {}) {
+                    Text("8:00am")
+                        .foregroundColor(.black)
+                        .font(.system(size: 32).bold())
+                        .underline()
+                }
+            }
+            Text("Alarms will ring every 5 min starting at 7:30am")
+            Button(action: {}) {
+                Text("Switch off today's alarms")
+                    .foregroundColor(.white)
+                    .font(.system(size: 28))
+                    .font(.headline)
+                    .padding(20)
+            }
+            .clipShape(Capsule())
+            .background(.green)
+        }
     }
 }
 
